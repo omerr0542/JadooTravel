191c0927dd294ce1a22a61d37437d1e3e56ec5db
omerr0542 <o.faruk0542@gmail.com>
2025-09-16 21:46:19 +0300

    Add project files.

---
+++ JadooTravel/Entites/Feature.cs
@@ -0,0 +1,11 @@
+﻿namespace JadooTravel.Entites
+{
+    public class Feature
+    {
+        public string FeatureId { get; set; }
+        public string Title { get; set; }
+        public string MainTitle { get; set; }
+        public string Description { get; set; }
+        public string VideoUrl { get; set; }
+    }
+}
---
+++ JadooTravel/Entites/TripPlan.cs
@@ -0,0 +1,10 @@
+﻿namespace JadooTravel.Entites
+{
+    public class TripPlan
+    {
+        public string TripPlanId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string IconUrl { get; set; }
+    }
+}
